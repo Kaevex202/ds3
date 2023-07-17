c8c39802b34c152d3db790a2a043520d3b332429
Kaevex <info@kymancheng.nl>
2023-07-17 22:20:32 +0200

    change

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,6 @@
-<div class="justify-center items-center flex flex-col mx-auto mt-16"><h1 class="text-4xl font-extrabold md:text-7xl mt-4 mb-8 2xl:mt-12 px-4 lg:px-0">UNDER CONSTRUCTION</h1></div>
+<div class=" flex flex-col mx-auto mt-16 items-center">
+    <h1 class="text-4xl font-extrabold md:text-7xl mt-4 mb-8 2xl:mt-12 px-4 lg:px-0 ">ABOUT</h1>
+    <div class="lg:w-3/5 ">
+        <p class="text-lg">Hi! I'm Kaevex, and I made SoulsBorneChallenges on a bored night. The site is still being finished so for now, but I would love to get your feedback. If you have any suggestions, feel free to contact me on Discord. (ussername: kaevex)</p>
+    </div>
+</div>
--- src/routes/api/auth/+server.ts
+++
@@ -1,2 +0,0 @@
-import lucia from 'lucia-auth'
-
