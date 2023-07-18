15068d5d5a8d4d22143f9fa506798bc1adc89e95
Kaevex <info@kymancheng.nl>
2023-07-19 01:19:32 +0200

    use cookies instead I guess

--- src/routes/auth/discord/callback/+page.svelte
+++ src/routes/auth/discord/callback/+page.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
-    import { avatarUrl, username, loggedIn} from '$lib/stores.js'
-    export let data;
 </script>
 
 <div>
-    {#await data}
-    {:then}
-    <p>Hi! {data.strapiResponse[0].username}.</p>
-    {/await}
+    <p>Hi! </p>
 </div>
