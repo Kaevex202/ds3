8be49dca0bd7d6ca040888b118512bd1eb9a23e9
Kaevex <info@kymancheng.nl>
2023-07-18 22:29:17 +0200

    extra options

--- src/lib/FAQ.svelte
+++ src/lib/FAQ.svelte
@@ -1,6 +1,14 @@
 <div class="justify-center flex flex-col mx-auto mt-16 text-left lg:w-1/2 lg:items-center px-6 pb-8">
     <h2 class="lg:w-3/5 flex text-left items-start text-4xl font-extrabold mt-4 mb-8 2xl:mt-12 ">FAQ</h2>
     <div class="lg:w-3/5 question items-start">
+        <p class="flex font-semibold ">What is this website used for?</p>
+        <p class="flex ">This website is created to give you randomized challenge runs in Fromsoft games. These range from somewhat easy, to quite challenging.</p>
+    </div>
+    <div class="lg:w-3/5 question mt-6 items-start">
+        <p class="flex font-semibold ">What are the rules for each restriction.</p>
+        <p class="flex ">The Challenge Runs are meant to provide you a fun challenge to these games. The rules are not very specific for this reason, some people might cheat in the weapon they have to use with the cheat ending, while others will rush to get that weapon and use nothing else. It is for you to decide how to stick to these rules. For the leaderboards, we'll be making separate strict rules when it launches.</p>
+    </div>
+    <div class="lg:w-3/5 question mt-6 items-start">
         <p class="flex font-semibold ">What if my challenge is impossible to fulfill?</p>
         <p class="flex">I'm working on filtering those out; for now, just reroll.</p>
     </div>
