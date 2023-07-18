8da18c4bdb7157ddf6a69c7e83bd916aa9175029
Kaevex <info@kymancheng.nl>
2023-07-19 01:54:06 +0200

    test

--- src/routes/auth/discord/callback/+page.svelte
+++ src/routes/auth/discord/callback/+page.svelte
@@ -2,7 +2,8 @@
     import type { PageData } from './$types';
     export let data: PageData;
 
-    console.log(data.strapiResponse.username);
+    console.log(data);
+    console.log(data.strapiResponse);
 </script>
 
 <div>
