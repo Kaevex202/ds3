eed4cd40bfb5457718268d3dbcaf44b79f0e1d08
Kaevex <info@kymancheng.nl>
2023-07-18 22:38:20 +0200

    fix select box bg color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 
 <section class=" flex-row mx-auto mt-16 ">
 	<h1 class="flex flex-col lg:flex-row justify-center items-center text-4xl font-extrabold md:text-7xl mt-4 mb-8 2xl:mt-12 px-4 lg:px-0"><label>
-        <select name="gameName" id="game" bind:value={game} class="underline hover:text-[#105D97]">
+        <select name="gameName" id="game" bind:value={game} class="underline hover:text-[#105D97] bg-[#FAF9F6]">
             <option value="ds3"class=" text-4xl font-extrabold md:text-7xl text-[#000000]">DARK SOULS 3</option>
             <option value="er" class=" text-4xl font-extrabold md:text-7xl text-[#000000]">ELDEN RING</option>
 			<option value="ds" class=" text-4xl font-extrabold md:text-7xl text-[#000000]">DARK SOULS</option>
