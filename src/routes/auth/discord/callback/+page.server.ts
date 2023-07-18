439f5d980f5858447e32b3333b9b994382bf2485
Kaevex <info@kymancheng.nl>
2023-07-18 16:51:19 +0200

    fix image url

--- src/routes/auth/discord/callback/+page.server.ts
+++ src/routes/auth/discord/callback/+page.server.ts
@@ -78,7 +78,7 @@ const discordinfo = await fetch('https://discord.com/api/users/@me', {
     blocked: false,
     password: "whatever",
     discordid: discordUserInfo.id,
-    avatarurl: `https://cdn.discordapp.com/avatars/${discordUserInfo.id}}/${discordUserInfo.avatars}}.png`,
+    avatarurl: `https://cdn.discordapp.com/avatars/${discordUserInfo.id}/${discordUserInfo.avatars}.png`,
     role: ''
   };
 
