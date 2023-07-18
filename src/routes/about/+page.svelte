95d7a539aaa6d26e68eb05c3645ac53371d7c081
Kaevex <info@kymancheng.nl>
2023-07-18 22:37:18 +0200

    update about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,7 @@
-<div class=" flex flex-col mx-auto mt-16 items-center">
+<div class=" flex flex-col mx-auto mt-16 items-center px-6 lg:px-0">
     <h1 class="text-4xl font-extrabold md:text-7xl mt-4 mb-8 2xl:mt-12 px-4 lg:px-0 ">ABOUT</h1>
-    <div class="lg:w-3/5 ">
-        <p class="text-lg">Hi! I'm Kaevex, and I made SoulsBorneChallenges on a bored night. The site is still being finished so for now, but I would love to get your feedback. If you have any suggestions, feel free to contact me on Discord. (ussername: kaevex)</p>
-    </div>
+    <div class="lg:w-4/5 xl:w-7/12 2xl:w-5/12">
+        <p class="text-lg">Hi! I'm Kaevex, and I made SoulsBorneChallenges on a bored night. The site is still being finished so for now, but I would love to get your feedback. If you have any suggestions, feel free to contact me on Discord. (username: kaevex)</p>
+    </div><br/>
+    <div class="lg:w-4/5 xl:w-7/12 2xl:w-5/12 text-lg"> If you want to see me try these challenges, come <a href="http://twitch.tv/kaevex" rel="nofollow" class="font-bold hover:text-[#105D97]">follow me on Twitch.</a> I'll be doing more of these once the site is fully up and running.</div>
 </div>
