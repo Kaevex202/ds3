d00300d83562024576a2b81864347a967f5cc19b
Kaevex <info@kymancheng.nl>
2023-07-18 14:01:16 +0200

    try again on logout

--- src/routes/auth/discord/signout/+page.server.ts
+++ src/routes/auth/discord/signout/+page.server.ts
@@ -3,15 +3,8 @@
  */
 export async function load({cookies}) {
     console.log('redirect to / with cleared cookies');
-    cookies.delete("disco_access_token")
-    cookies.delete('disco_refresh_token')
+    cookies.delete("disco_access_token");
+    cookies.delete('disco_refresh_token');
     return {
-      headers: {
-        'set-cookie': [
-          `disco_access_token=deleted; Path=/; Max-Age=-1`,
-          `disco_refresh_token=deleted; Path=/; Max-Age=-1`,
-        ],
-        Location: '/'
-      },
-      status: 302
+        ok:true,
     }}
