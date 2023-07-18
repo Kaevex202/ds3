9f937003fd9b4789eec6826c089e5ed2a10da052
Kaevex <info@kymancheng.nl>
2023-07-18 14:03:57 +0200

    add extra info

--- src/routes/auth/discord/signout/+page.server.ts
+++ src/routes/auth/discord/signout/+page.server.ts
@@ -3,8 +3,16 @@
  */
 export async function load({cookies}) {
     console.log('redirect to / with cleared cookies');
-    cookies.delete("disco_access_token");
-    cookies.delete('disco_refresh_token');
+    cookies.delete("disco_access_token", { httpOnly: true,
+        sameSite: 'strict',
+        secure: false,
+        path: '/',
+     });
+    cookies.delete('disco_refresh_token', { httpOnly: true,
+        sameSite: 'strict',
+        secure: false,
+        path: '/',
+     });
     return {
         ok:true,
     }}
