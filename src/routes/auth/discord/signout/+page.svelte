2e66d152c2c6e0eb848ac9ecf1f3527bb4e5e39e
Kaevex <info@kymancheng.nl>
2023-07-19 22:18:35 +0200

    logout redirect

--- src/routes/auth/discord/signout/+page.svelte
+++ src/routes/auth/discord/signout/+page.svelte
@@ -1,1 +1,14 @@
-<h1>Logged out</h1>
+<script>
+    import { onMount } from "svelte";
+    import {goto} from '$app/navigation'
+
+    let timeout = 900;
+
+onMount(() => {
+    setTimeout(() => {
+        goto('/')
+    }, timeout);
+  });
+
+</script>
+<h1 class="flex text-4xl font-extrabold md:text-7xl mt-4 mb-8 2xl:mt-12 px-4 lg:px-0 justify-center mx-auto">Logged out</h1>
