e0911a262293e6798b2e52ae539758f2ba406222
Kaevex <info@kymancheng.nl>
2023-07-19 21:52:00 +0200

    test localstorage

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,7 +1,10 @@
 import { writable } from 'svelte/store';
 
 /** SETTINGS MENU */
-export const username = writable("");
+export const storedUsername = localStorage.username;
+export const username = writable(storedUsername || "");
+username.subscribe((value) => localStorage.content = value)
+
 export const avatarUrl = writable("");
 export const discordid = writable("");
 export const loggedIn = writable("false");
