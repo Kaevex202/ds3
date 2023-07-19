f628648fb31c1202fae387e2bf94d0c99a28b16b
Kaevex <info@kymancheng.nl>
2023-07-20 01:31:28 +0200

    add redirect for login

--- src/routes/auth/discord/callback/+page.svelte
+++ src/routes/auth/discord/callback/+page.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
     import { username, avatarurl , loggedIn } from '$lib/localstores'
+    import {goto} from '$app/navigation'
     import type { PageData } from './$types';
     export let data: PageData;
     import { onMount } from 'svelte';
+
+    let timeout = 900;
 
     onMount(async () => {
 
             $username = data.discordUserInfo.username;
             $avatarurl = `https://cdn.discordapp.com/avatars/${data.discordUserInfo.id}/${data.discordUserInfo.avatar}.png`
             $loggedIn = "true";
+
+                setTimeout(() => {
+                goto('/')
+            }, timeout);
         }
     );
 
