817547d340182767202cbb089e67ff528e49a9c4
Kaevex <info@kymancheng.nl>
2023-07-19 03:15:25 +0200

    using data from discorduserinfo instead of strapiuserinfo.

--- src/routes/auth/discord/callback/+page.svelte
+++ src/routes/auth/discord/callback/+page.svelte
@@ -6,6 +6,12 @@
 
     console.log(data);
 
+    username.set(data.discordUserInfo.username);
+    discordid.set(data.discordUserInfo.id);
+    avatarUrl.set(`https://cdn.discordapp.com/avatars/${data.discordUserInfo.id}/${data.discordUserInfo.avatar}.png`);
+    loggedIn.set("true")
+
+
 </script>
 
 <div>
