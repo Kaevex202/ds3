7f41a4271e4d39b576f543a4231463bb855a26d4
Kaevex <info@kymancheng.nl>
2023-07-19 02:28:04 +0200

    test

--- src/routes/auth/discord/callback/+page.svelte
+++ src/routes/auth/discord/callback/+page.svelte
@@ -2,14 +2,7 @@
     import { username, avatarUrl, discordid, loggedIn } from '$lib/stores'
     import type { PageData } from './$types';
     export let data: PageData;
-
-    console.log(data);
-    console.log(data.strapiResponse);
-    console.log(data.strapiResponse[0]);
-    console.log(data.strapiResponse[0].discordid);
-    console.log(data.strapiResponse[0].email);
-    console.log(data.strapiResponse.email);
-
+    
     username.set(data.strapiResponse[0].username)
     avatarUrl.set(data.strapiResponse[0].avatarUrl)
 </script>
