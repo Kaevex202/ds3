ea55a221da8251dac2881f3a0ffd634de1248f45
Kaevex <info@kymancheng.nl>
2023-07-19 03:09:14 +0200

    log discord info

--- src/routes/auth/discord/callback/+page.server.ts
+++ src/routes/auth/discord/callback/+page.server.ts
@@ -104,8 +104,7 @@ const discordinfo = await fetch('https://discord.com/api/users/@me', {
 
   }
 
-  console.log(strapiResponse);
-  console.log(strapiResponse.strapiResponse);
+  console.log(discordUserInfo);
 
   if (strapiResponse.length > 0){
 
