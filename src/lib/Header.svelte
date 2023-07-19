35f381dbb4ca2bc09062585804486967359806e9
Kaevex <info@kymancheng.nl>
2023-07-19 20:29:09 +0200

    test

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -10,19 +10,20 @@
 </script>
 
 <header>
-
-	<nav class="inline-flex lg:flex items-center justify-between flex-wrap p-6 w-full lg:w-[85%] mx-auto">
+	<div>
+	<nav class="inline-flex lg:flex items-start justify-between flex-wrap p-6 w-full lg:w-[85%] mx-auto h-min">
 		<div id="logo" class="flex items-center flex-shrink-0 text-white mr-6 flex-[100] w-fit h-1/2">
-			<a href="/" aria-current={$page.url.pathname === '/' ? 'page' : undefined}><h1 class="w-full hidden lg:flex lg:text-4xl font-black px-6 lg:px-0 text-black">SOULSBORNECHALLENGES</h1></a>
+			<div id="loggedIn" class="mr-8 flex flex-row items-center border px-4 py-4 rounded-xl hover:bg-[#A7C7E7] text-black"><img src="https://cdn.discordapp.com/avatars/125998088409055233/7943dcc69762855669986f04e3e9bb7f.png" alt="your Discord profile" class="rounded-full mr-4" height=40 width=40 /><h4 class="font-bold text-lg">Kaevex</h4></div>
+			<a href="/" aria-current={$page.url.pathname === '/' ? 'page' : undefined}><h1 class="w-full hidden lg:flex lg:text-4xl font-black px-6 lg:px-0 text-black ">SOULSBORNECHALLENGES</h1></a>
 			<a href="/" aria-current={$page.url.pathname === '/' ? 'page' : undefined}><h1 class="w-full flex lg:hidden text-lg text-black font-black items-start">SOULSBORNECHALLENGES</h1></a>
+			
 		</div>
 
 
 	
 
-		<div class="w-full block flex-grow hidden lg:flex lg:items-center lg:w-auto font-medium">
-			<div class="text-sm lg:flex-grow inline-flex" >
-				{#if $username.length > 0 }<img src={$avatarUrl} alt="your Discord profile"/><h4>{$username}</h4>{/if}
+		<div class="w-full block flex-grow hidden lg:flex lg:w-auto font-medium ">
+			<div class="text-sm lg:flex-grow inline-flex items-center mt-5" >
 				<a href="/about" class="block mt-4 lg:inline-block lg:mt-0 text-[#000] hover:text-[#105D97] mr-12 text-lg" aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				About
 				</a>
@@ -32,12 +33,13 @@
 				<a href="/challenge" class="block mt-4 lg:mt-0 text-[#000] hover:text-[#105D97] mr-12 text-lg" aria-current={$page.url.pathname.startsWith('/challenge') ? 'page' : undefined} >
 				Challenge of the Month
 				</a>
-				<a href="/contact" class="text-base font-semibold inline-block px-4 py-2 leading-none border rounded text-[#000] border-[#000] hover:border-[#105D97] hover:text-[#105D97] hover:bg-[#fff] mt-4 lg:mt-0" aria-current={$page.url.pathname.startsWith('/contact') ? 'page' : undefined}>Contact</a>
+				<a href="/contact" class="text-base font-semibold inline-block px-4 py-2 h-min leading-none border rounded text-[#000] border-[#000] hover:border-[#105D97] hover:text-[#105D97] hover:bg-[#fff] mt-4 lg:mt-0" aria-current={$page.url.pathname.startsWith('/contact') ? 'page' : undefined}>Contact</a>
 			</div>
 		</div>
 		<div class="lg:hidden"><Hamburger bind:open /></div></nav>
 
 		<MobileMenu bind:open />
+		
 </header>
 
 <style>
--- src/lib/stores.js
+++ src/lib/stores.js
@@ -3,5 +3,4 @@ import { writable } from 'svelte/store';
 /** SETTINGS MENU */
 export const username = writable("");
 export const avatarUrl = writable("");
-export const discordid = writable("");
 export const loggedIn = writable("false");
