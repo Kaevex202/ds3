c9193bbc49e501d469a2be891911228b0730e432
Kaevex <info@kymancheng.nl>
2023-07-19 02:34:44 +0200

    test

--- src/routes/auth/discord/callback/+page.svelte
+++ src/routes/auth/discord/callback/+page.svelte
@@ -2,11 +2,10 @@
     import { username, avatarUrl, discordid, loggedIn } from '$lib/stores'
     import type { PageData } from './$types';
     export let data: PageData;
-    
-    username.set(data.strapiResponse[0].username)
-    avatarUrl.set(data.strapiResponse[0].avatarUrl)
+
+    console.log(data);
+
 </script>
 
 <div>
-    <p>Hi! {$username}</p><img src={$avatarUrl}/>
 </div>
