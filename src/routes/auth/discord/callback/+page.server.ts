69925be595458e3b6dc20036c1ec5aab2ba20066
Kaevex <info@kymancheng.nl>
2023-07-19 02:48:31 +0200

    test

--- src/routes/auth/discord/callback/+page.server.ts
+++ src/routes/auth/discord/callback/+page.server.ts
@@ -106,7 +106,6 @@ const discordinfo = await fetch('https://discord.com/api/users/@me', {
 
   console.log(strapiResponse);
   console.log(strapiResponse.strapiResponse);
-  console.log(strapiResponse.strapiResponse.username);
   console.log(strapiResponse.strapiResponse[0].username);
 
   if (strapiResponse.length > 0){
