ed290c87c56e447f6755f6a86b2d2bdb77a77adf
Kaevex <info@kymancheng.nl>
2023-07-20 00:56:56 +0200

    test

--- src/lib/localstores.js
+++ src/lib/localstores.js
@@ -3,5 +3,5 @@ import {writable} from 'svelte/store'
 
 export const usernames = writable(browser && (localStorage.getItem("usernames")));
 
-usernames.subscribe((val) => browser && localStorage.setItem("usernames", val || "" ))
+usernames.subscribe((value) => browser && (localStorage.usernames = value));
 
