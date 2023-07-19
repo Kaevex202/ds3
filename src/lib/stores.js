1f052eac4f44851b72c133a2d974bb74f1965185
Kaevex <info@kymancheng.nl>
2023-07-19 21:18:29 +0200

    changes

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,6 +1,12 @@
 import { writable } from 'svelte/store';
 
-/** SETTINGS MENU */
-export const username = writable("");
+/** STORING USERNAME */
+    const storedUsername = localStorage.getItem("username");
+    export const username = writable(storedUsername);
+    username.subscribe(value => {
+        localStorage.set("username", value);
+    });
+
+
 export const avatarUrl = writable("");
 export const loggedIn = writable("false");
