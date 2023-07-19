a290f14d45478594d5d13ed6111a0c17a1717804
Kaevex <info@kymancheng.nl>
2023-07-19 22:01:32 +0200

    test

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,10 +1,7 @@
 import { writable } from 'svelte/store';
 
 /** SETTINGS MENU */
-export const storedUsername = localStorage.username;
-export const username = writable(storedUsername || "");
-username.subscribe((value) => localStorage.content = value)
-
+export const username = writable("");
 export const avatarUrl = writable("");
 export const discordid = writable("");
 export const loggedIn = writable("false");
--- src/routes/auth/discord/callback/+page.svelte
+++ src/routes/auth/discord/callback/+page.svelte
@@ -2,10 +2,21 @@
     import { username, avatarUrl, loggedIn } from '$lib/stores'
     import type { PageData } from './$types';
     export let data: PageData;
+    import {browser} from '$app/environment'
+    import { onMount } from 'svelte';
+
+    onMount(async () => {
+        if(browser){
+        localStorage.set("username", data.discordUserInfo.username || "");
 
     username.set(data.discordUserInfo.username);
     avatarUrl.set(`https://cdn.discordapp.com/avatars/${data.discordUserInfo.id}/${data.discordUserInfo.avatar}.png`);
     loggedIn.set("true")
+    }
+    });
+
+
+
 
 
 </script>
