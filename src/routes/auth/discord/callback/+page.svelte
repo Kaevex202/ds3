831637e74e958fc1ad0208d5591e872b34323ce4
Kaevex <info@kymancheng.nl>
2023-07-19 20:30:09 +0200

    fix discordid still being used

--- src/routes/auth/discord/callback/+page.svelte
+++ src/routes/auth/discord/callback/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-    import { username, avatarUrl, discordid, loggedIn } from '$lib/stores'
+    import { username, avatarUrl, loggedIn } from '$lib/stores'
     import type { PageData } from './$types';
     export let data: PageData;
 
     username.set(data.discordUserInfo.username);
-    discordid.set(data.discordUserInfo.id);
     avatarUrl.set(`https://cdn.discordapp.com/avatars/${data.discordUserInfo.id}/${data.discordUserInfo.avatar}.png`);
     loggedIn.set("true")
 
