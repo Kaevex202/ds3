4bea7097252aff4b9eb0950a24ef1bdf44f0ba23
Kaevex <info@kymancheng.nl>
2023-07-19 21:42:07 +0200

    test

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -56,8 +56,8 @@
 			class="mr-8 flex flex-row items-center border px-4 py-2 rounded-xl bg-blue-200 hover:bg-blue-300 focus:ring-2 focus:outline-none focus:ring-blue-300 text-black"
 			type="button"
 			>
-				<img src="https://cdn.discordapp.com/avatars/125998088409055233/${avatarUrl}.png" alt="your Discord profile" class="rounded-full mr-4" height=40 width=40 />
-				<h4 class="font-bold text-lg">{username}</h4>
+				<img src="{$avatarUrl}" alt="your Discord profile" class="rounded-full mr-4" height=40 width=40 />
+				<h4 class="font-bold text-lg">{$username}</h4>
 				<svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
 					<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
 				</svg>
