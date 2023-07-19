05fda055eaf875da983b633aa2e6657f9628d180
Kaevex <info@kymancheng.nl>
2023-07-19 21:49:35 +0200

    test

--- src/lib/LoggedInIcon.svelte
+++ src/lib/LoggedInIcon.svelte
@@ -1,5 +1,5 @@
 <script>
-import { username , avatarUrl , loggedIn} from './stores';
+import { username , avatarUrl} from './stores';
 
 let dropdownExpanded = false;
 
@@ -7,3 +7,30 @@ function expandMenu(){
     dropdownExpanded = !dropdownExpanded
 }
 </script>
+
+<div id="userInfo" >
+    <button
+    on:click={expandMenu}
+    id="loggedIn"
+    class="mr-8 flex flex-row items-center border px-4 py-2 rounded-xl bg-blue-200 hover:bg-blue-300 focus:ring-2 focus:outline-none focus:ring-blue-300 text-black"
+    type="button"
+    >
+        <img src="{$avatarUrl}" alt="your Discord profile" class="rounded-full mr-4" height=40 width=40 />
+        <h4 class="font-bold text-lg">{$username}</h4>
+        <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+        </svg>
+    </button>
+    {#if dropdownExpanded}
+        <div id="dropdown" class="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 absolute">
+            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" >
+            <li>
+                <a href="/account" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" on:click={expandMenu}>Account</a>
+            </li>
+            <li>
+                <a href="/auth/discord/signout" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" on:click={expandMenu}>Logout</a>
+            </li>
+            </ul>
+        </div>
+    {/if}
+</div>
