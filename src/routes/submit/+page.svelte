f4faed3ede2615772846b410d1093c0b0e1e0fee
Kaevex <info@kymancheng.nl>
2023-07-20 12:26:32 +0200

    Make entire submit button clickable

--- src/routes/submit/+page.svelte
+++ src/routes/submit/+page.svelte
@@ -117,9 +117,6 @@
         <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Comment</label>
         <textarea id="message" rows="4" class="mb-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
         <div class="flex justify-end">
-            <div class="flex w-min text-base font-semibold inline-block px-6 py-4 leading-none border rounded text-[#000] border-[#000] hover:border-[#105D97] hover:text-[#105D97] hover:bg-[#fff] mt-4 ">
-            <button>Submit</button>
-            </div>
+            <button id="submitButton"><span class="block w-min text-base font-semibold inline-block px-6 py-4 leading-none border rounded text-[#000] border-[#000] hover:border-[#105D97] hover:text-[#105D97] hover:bg-[#fff] mt-4 ">Submit</span></button>
         </div>
-
 </div>
