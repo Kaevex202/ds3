84e00a9e5276adbaf116742728265d6701d78c20
Kyman <info@kymancheng.nl>
2023-07-21 01:27:03 +0200

    clarify bounty

--- src/routes/challengehidden/+page.svelte
+++ src/routes/challengehidden/+page.svelte
@@ -32,7 +32,7 @@
             <div id="challengerun" class="flex flex-col mb-20 items-center">
                 <h2 class="text-4xl font-extrabold ">Dark Souls 3 - Midora%</h2><br/>
                 <div id="challengeInfo" class="flex flex-col w-9/12 justify-between">
-                <div class="inline-flex justify-between"><p class="font-semibold">Bounty:</p><p>{bounty}</p></div>
+                <div class="inline-flex justify-between"><p class="font-semibold">Bounty (fastest IGT):</p><p>{bounty}</p></div>
                 <div class="inline-flex justify-between"><p class="font-semibold">Category:</p><p>{category}</p></div>
                 <div class="inline-flex justify-between"><p class="font-semibold">Glitchless?:</p><p>{glitches}</p></div>
                 <div class="inline-flex justify-between"><p class="font-semibold">Starting Class:</p><p>{startingClass}</p></div>
