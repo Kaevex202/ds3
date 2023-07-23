bdd0f49d53377a4596116015920dc816a7ed360d
Kaevex <info@kymancheng.nl>
2023-07-23 21:49:04 +0200

    updateurl based on game selected.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
-<script>
+<script lang="ts">
 	import Challenge from "$lib/Challenge.svelte";
 	import Faq from "$lib/FAQ.svelte";
 	import {page} from '$app/stores'
 
 	let urlParams = $page.url.searchParams;
 	let game = urlParams.get('game') || "ds3";
+
+	function updateURL(){
+		window.history.replaceState(history.state, '', `?game=`+game)
+	}
 </script>
 
 <svelte:head>
@@ -14,7 +18,7 @@
 
 <section class=" flex-row mx-auto mt-16 ">
 	<h1 class="flex flex-col lg:flex-row justify-center items-center text-4xl font-extrabold md:text-5xl 2xl:text-7xl mt-4 mb-8 2xl:mt-12 px-4 lg:px-0"><label>
-        <select name="gameName" id="game" bind:value={game} class="underline hover:text-[#105D97] bg-[#FAF9F6]">
+        <select name="gameName" id="game" bind:value={game} on:change={updateURL} class="underline hover:text-[#105D97] bg-[#FAF9F6]">
             <option value="ds3"class=" text-4xl font-extrabold md:text-7xl text-[#000000]">DARK SOULS 3</option>
             <option value="er" class=" text-4xl font-extrabold md:text-7xl text-[#000000]">ELDEN RING</option>
 			<option value="ds" class=" text-4xl font-extrabold md:text-7xl text-[#000000]">DARK SOULS</option>
