325d52b37c8ceb6712418d9810dd74b1c8ba6bc0
Kaevex <info@kymancheng.nl>
2023-07-25 12:15:15 +0200

    google analytics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,9 @@
 	let urlParams = $page.url.searchParams;
 	let game = urlParams.get('game') || "ds3";
 
-	function updateURL(){
-		window.history.replaceState(history.state, '', `?game=`+game)
-	}
+	//function updateURL(){
+	//	window.history.replaceState(history.state, '', `?game=`+game)
+	//}
 </script>
 
 <svelte:head>
@@ -18,7 +18,7 @@
 
 <section class=" flex-row mx-auto mt-16 ">
 	<h1 class="flex flex-col lg:flex-row justify-center items-center text-4xl font-extrabold md:text-5xl 2xl:text-7xl mt-4 mb-8 2xl:mt-12 px-4 lg:px-0"><label>
-        <select name="gameName" id="game" bind:value={game} on:change={updateURL} class="underline hover:text-[#105D97] bg-[#FAF9F6]">
+        <select name="gameName" id="game" bind:value={game}  class="underline hover:text-[#105D97] bg-[#FAF9F6]">
             <option value="ds3"class=" text-4xl font-extrabold md:text-7xl text-[#000000]">DARK SOULS 3</option>
             <option value="er" class=" text-4xl font-extrabold md:text-7xl text-[#000000]">ELDEN RING</option>
 			<option value="ds" class=" text-4xl font-extrabold md:text-7xl text-[#000000]">DARK SOULS</option>
