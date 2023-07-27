9a37e3321350f0604eed9bf444f61611bb656f38
Kaevex <info@kymancheng.nl>
2023-07-27 13:36:17 +0200

    update verified runs thing

--- src/routes/testpages/livesplitparser/+page.server.ts
+++ src/routes/testpages/livesplitparser/+page.server.ts
@@ -1,6 +1,12 @@
 import { XMLParser, XMLBuilder, XMLValidator} from "fast-xml-parser";
 
-const parser = new XMLParser();
+
+const options = {
+        ignoreAttributes: false,
+        // alwaysCreateTextNode: true
+    };
+const parser = new XMLParser(options);
+
 let parsedJson;
 
 export async function load({}) {
@@ -8,7 +14,7 @@ fetch('https://pastebin.com/raw/my3YZnZg')
         .then(response => response.text())
         .then(xmlString => parser.parse(xmlString))
         .then(data => parsedJson = data)
-        .then(data => console.log(parsedJson))
+        .then(data => console.log(parsedJson.Run.Segments.Segment[5].SegmentHistory.Time[5]["@_id"]))
 
 return parsedJson;
 }
--- src/routes/testpages/livesplitparser/+page.svelte
+++ src/routes/testpages/livesplitparser/+page.svelte
@@ -2,11 +2,51 @@
 	import { onMount } from 'svelte';
 
     export let data;
+    let sumOfBest = 0;
     let isLoaded = false;
 
+    function getTimeDuration(time){
+        var tHours = time[0]*1000*60*60
+        var tMins = time[1]*1000*60
+        var tSecs = time[2].split(".")[0]*1000
+        var tMilisecs = time[2].split(".")[1]
+
+        var t = +tHours + +tMins + +tSecs + +tMilisecs;
+
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+
+
+        return {
+        'total': t,
+        'days': days,
+        'hours': ('0'+hours).slice(-2),
+        'minutes': ('0'+minutes).slice(-2),
+        'seconds': ('0'+seconds).slice(-2),
+        'miliseconds':('00'+tMilisecs).slice(-3)
+        };
+    }
+
+    function milisecondsToTime(t){
+        return{
+        'days': ('0'+ +Math.floor(t / (1000 * 60 * 60 * 24))).slice(-2),
+        'hours': ('0'+ +Math.floor((t / (1000 * 60 * 60)) % 24)).slice(-2),
+        'minutes': ('0'+ +Math.floor((t / 1000 / 60) % 60)).slice(-2),
+        'seconds': ('0'+ +Math.floor((t / 1000) % 60)).slice(-2),
+        }
+    }
+
     onMount(async ()=>{
-        console.log(data.Run.Segments.Segment[0].SegmentHistory.Time);
+
+        console.log(data.Run.Segments.Segment)
+
         isLoaded = true;
+
+        data.Run.Segments.Segment.forEach(element => {
+            sumOfBest = +sumOfBest + +getTimeDuration(element.BestSegmentTime.GameTime.slice(0,-4).split(":")).total;
+        });
     })
 </script>
 <div class="ml-16">
@@ -15,10 +55,12 @@
         <p>Game: {data.Run.GameName}</p>
         <p>Category: {data.Run.CategoryName}</p>
         <p>Attempts: {data.Run.AttemptCount}</p>
+        <p>Completed Attempts: {data.Run.AttemptHistory.Attempt.filter(Boolean).length}</p>
+        <p>Sum of Best: {milisecondsToTime(sumOfBest).hours}:{milisecondsToTime(sumOfBest).minutes}:{milisecondsToTime(sumOfBest).seconds}</p>
 <br/><br/>
         <h3 class="font-bold text-xl">Segments</h3>
     {#each data.Run.Segments.Segment as segment}
-        <p>{segment.Name}</p><p>Best Segment Time: {segment.BestSegmentTime.GameTime.slice(0,-5)}</p>
+        <div><p>{segment.Name}</p><p>Best Segment Time: {getTimeDuration(segment.BestSegmentTime.GameTime.slice(0,-4).split(":")).minutes}:{getTimeDuration(segment.BestSegmentTime.GameTime.slice(0,-4).split(":")).seconds}.{getTimeDuration(segment.BestSegmentTime.GameTime.slice(0,-4).split(":")).miliseconds}</p></div>
     {/each}
     {/if}
 </div>
