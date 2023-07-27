cff33033b709ee57b165ed2e8887b83426b34688
Kyman <info@kymancheng.nl>
2023-07-27 20:29:22 +0200

    livesplitparser

--- src/routes/testpages/livesplitparser/+page.server.ts
+++ src/routes/testpages/livesplitparser/+page.server.ts
@@ -1,20 +1,22 @@
 import { XMLParser, XMLBuilder, XMLValidator} from "fast-xml-parser";
 
-
 const options = {
-        ignoreAttributes: false,
-        // alwaysCreateTextNode: true
-    };
+    ignoreAttributes: false,
+    attributeNamePrefix : "@_",
+    allowBooleanAttributes: true
+};
 const parser = new XMLParser(options);
-
 let parsedJson;
 
-export async function load({}) {
-fetch('https://pastebin.com/raw/my3YZnZg')
-        .then(response => response.text())
-        .then(xmlString => parser.parse(xmlString))
-        .then(data => parsedJson = data)
-        .then(data => console.log(parsedJson.Run.Segments.Segment[5].SegmentHistory.Time[5]["@_id"]))
 
-return parsedJson;
+export const actions ={
+    default: async({request, fetch}) =>{
+        const formData = await request.formData()
+        const formObjectData = Object.fromEntries(formData)
+        const res = await fetch(`${formObjectData.splitUrl}`)
+        const xmlString = await res.text();
+        const parsedData = parser.parse(xmlString)
+        console.log(parsedData)
+        return parsedData;
+    }
 }
--- src/routes/testpages/livesplitparser/+page.svelte
+++ src/routes/testpages/livesplitparser/+page.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+    import { enhance } from '$app/forms';
 
-    export let data;
+
+    export let form;
     let sumOfBest = 0;
     let isLoaded = false;
-    let openPersonalbest = false;
     let completedRuns = [];
     let fastestRun;
+    let fastestRunMiliSec;
+    let formLoaded = false;
+
+    $:  submission_status = form?.Run?.AttemptCount
 
     //Turn array of Hr,Mins,Sec,Milisec into t('miliseconds') and then convert it back.
     function getTimeDuration(time){
@@ -44,55 +49,84 @@
     }
 
     function getPersonalBest(){
-        data.Run.AttemptHistory.Attempt.forEach(element =>{
+        form?.Run.AttemptHistory.Attempt.forEach(element =>{
             if(element.GameTime !== undefined){
                 completedRuns.push({id:element["@_id"],gameTime:getTimeDuration(element.GameTime.slice(0,-4).split(":")).total});
             }})
-            let fastestRunMiliSec = completedRuns.reduce(function(prev, curr) {
+            fastestRunMiliSec = completedRuns.reduce(function(prev, curr) {
                 return prev.gameTime < curr.gameTime ? prev : curr;
             })
             fastestRun = (milisecondsToTime(fastestRunMiliSec.gameTime));
-        console.log(completedRuns.length)
+            console.log(fastestRunMiliSec)
         return {
             'completedAttempts':completedRuns.length,
             'personalBest':`${fastestRun.hours}:${fastestRun.minutes}:${fastestRun.seconds}`,
         }
     }
 
-    //to make clicken open actually open up the times.
-    function clickOpenPersonalBest(){
-        openPersonalbest = !openPersonalbest;
+    function getPersonalBestSegment(i){
+        let split = form?.Run.Segments.Segment[i].SegmentHistory.Time.filter(x => x['@_id']===fastestRunMiliSec.id)[0].GameTime.slice(0,-4).split(":");
+        let time = getTimeDuration(split);
+        return{
+            'hours':time.hours,
+            'minutes':time.minutes,
+            'seconds':time.seconds,
+            'miliseconds': (split[2].split(".")[1])
+        }
     }
 
     onMount(async ()=>{
 
+        if(form?.Run?.AttemptCount > 0){
+            formLoaded = true;
+        }
         isLoaded = true;
         
         //Get Each Best Segment Time and add it up.
-        data.Run.Segments.Segment.forEach(element => {
+        form?.Run.Segments.Segment.forEach(element => {
             sumOfBest = +sumOfBest + +getTimeDuration(element.BestSegmentTime.GameTime.slice(0,-4).split(":")).total;
         });
     })
 </script>
 <div class="ml-16 mb-32">
     {#if isLoaded == true}
-        <h1>Hi</h1>
-        <p>Game: {data.Run.GameName}</p>
-        <p>Category: {data.Run.CategoryName}</p>
-        <p>Attempts: {data.Run.AttemptCount}</p>
+        <h1 class="text-5xl font-black mb-8 mt-16">Livesplit Parser</h1>
+        <form method="POST" class="flex flex-row"><label class="mr-4" for="splitUrl" >Splits URL:</label><input name="splitUrl" type="url" class="block mb-2 text-sm font-medium text-gray-900 border-2 w-1/2"/><button class="align-center flex ml-4 border-2 rounded h-fit px-2 bg-gray-100 font-semibold">Submit</button></form>
+        {#if formLoaded == true}
+        <p>Game: {form?.Run.GameName}</p>
+        <p>Category: {form?.Run.CategoryName}</p>
+        <p>Attempts: {form?.Run.AttemptCount}</p>
         <p>Completed Attempts: {getPersonalBest().completedAttempts}</p>
         <p>Personal Best: {getPersonalBest().personalBest}</p>
         <p>Sum of Best: {milisecondsToTime(sumOfBest).hours}:{milisecondsToTime(sumOfBest).minutes}:{milisecondsToTime(sumOfBest).seconds}</p>
-<br/><br/>
-        <h3 class="font-bold text-xl">Best Segments</h3>
-    <button on:click={clickOpenPersonalBest}><h4>OPEN</h4></button>
-        {#if openPersonalbest == true}
-        <div class="flex flex-col w-1/3">
-            <div class="flex flex-row justify-between font-bold"><div class="splitName">Name:</div><div class="time">Time:</div></div>
-            {#each data.Run.Segments.Segment as segment}
-                <div class="flex flex-row justify-between"><div class="boss flex"><p>{segment.Name}</p></div><div class="bestTime"><p>{getTimeDuration(segment.BestSegmentTime.GameTime.slice(0,-4).split(":")).minutes}:{getTimeDuration(segment.BestSegmentTime.GameTime.slice(0,-4).split(":")).seconds}.{getTimeDuration(segment.BestSegmentTime.GameTime.slice(0,-4).split(":")).miliseconds}</p></div></div>
-            {/each}
-        </div>
         {/if}
+<br/><br/>
+{#if formLoaded == true}
+        <div id="bestSegments" class="flex flex-col w-1/2">
+            <div class="flex flex-row gap-32">
+                <div class="flex flex-col">
+                    <div class="block font-bold text-lg">Details</div>
+                    <div class="name font-bold" >Name</div>
+                    {#each form?.Run.Segments.Segment as segment}
+                    <div class="boss flex"><p>{segment.Name}</p></div>
+                    {/each}
+                </div>
+                <div class="flex flex-col text-right">
+                    <div><h3 class="font-bold text-lg ">Best Segments</h3></div>
+                    <div class="time font-bold">Time:</div>
+                    {#each form?.Run.Segments.Segment as segment}
+                    <div class="bestTime  font-mono"><p>{getTimeDuration(segment.BestSegmentTime.GameTime.slice(0,-4).split(":")).minutes}:{getTimeDuration(segment.BestSegmentTime.GameTime.slice(0,-4).split(":")).seconds}.{getTimeDuration(segment.BestSegmentTime.GameTime.slice(0,-4).split(":")).miliseconds}</p></div>
+                    {/each}
+                </div>
+                <div class="flex flex-col text-right">
+                    <div><h3 class="font-bold text-lg">Personal Best</h3></div>
+                    <div class="time font-bold">Time:</div>
+                    {#each form?.Run.Segments.Segment as segment,i}
+                    <div class="personalBest text-right font-mono"><p>{getPersonalBestSegment(i).minutes}:{getPersonalBestSegment(i).seconds}.{getPersonalBestSegment(i).miliseconds}</p></div>
+                    {/each}
+                </div>
+            </div>
+        </div>
+    {/if}
     {/if}
 </div>
